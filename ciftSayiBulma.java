65243ae3e1030b7040a7774d03f45e35a9040241
Onur Cura <onurcuraa@gmail.com>
2022-11-09 00:05:07 +0300

    Proje yapıldı.

---
+++ ciftSayiBulma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ciftSayiBulma {
+    public static void main(String[] args) {
+        int i =0, j,top=0,n=0,ort;;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lütfen Değer Giriniz : ");
+        j = inp.nextInt();
+        while(i<=j){
+            if(i%3==0&&i%4==0&&i!=0){
+
+                top+=i;
+                n+=1;
+            }
+            i++;
+        }
+        ort=top/n;
+
+        System.out.println("Sıfırdan Girdiğiniz Değere Kadar Üçe ve Dörde Kalansız Bölünen Sayıların Ortalaması : " +ort);
+
+    }
+}
