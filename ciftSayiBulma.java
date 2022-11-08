281d7e097c3bb8d634483721d54ce6bc2203e7ca
Onur Cura <onurcuraa@gmail.com>
2022-11-09 00:11:42 +0300

    Düzeltme yapıldı.

--- ciftSayiBulma.java
+++ ciftSayiBulma.java
@@ -7,17 +7,22 @@ public class ciftSayiBulma {
 
         System.out.print("Lütfen Değer Giriniz : ");
         j = inp.nextInt();
+        System.out.print("Sıfırdan Girdiğiniz Değere Kadar İkiye Kalansız Bölünen Sayılar : " );
         while(i<=j){
             if(i%3==0&&i%4==0&&i!=0){
 
                 top+=i;
                 n+=1;
             }
+            if(i%2==0&&i!=0){
+                System.out.print(i + ",");
+            }
             i++;
         }
         ort=top/n;
+        System.out.println();
 
-        System.out.println("Sıfırdan Girdiğiniz Değere Kadar Üçe ve Dörde Kalansız Bölünen Sayıların Ortalaması : " +ort);
+        System.out.println( "Sıfırdan Girdiğiniz Değere Kadar Üçe ve Dörde Kalansız Bölünen Sayıların Ortalaması : " +ort);
 
     }
 }
