d04aa84fea7dba3a1229de80eff8960331380872
Onur Cura <onurcuraa@gmail.com>
2022-11-10 14:13:02 +0300

    Proje yapıldı.

---
+++ pratikUsluSayi.java
@@ -0,0 +1,22 @@
+import  java.util.Scanner;
+
+public class pratikUsluSayi {
+    public static void main(String[] args) {
+        int a,b,total=1;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayı :");
+        a= inp.nextInt();
+        System.out.print("Üssü olacak sayı :");
+        b=inp.nextInt();
+
+        for(int i=1;i<=b;i++){
+            total *=a;
+
+        }
+        System.out.print("Cevap : " + total);
+
+
+
+    }
+}
