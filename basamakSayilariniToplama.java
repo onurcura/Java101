6c326ef0eacad44eb82da6a31a5856ca7f1401c2
Onur Cura <onurcuraa@gmail.com>
2022-11-10 15:26:09 +0300

    Proje yapıldı.

---
+++ basamakSayilariniToplama.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class basamakSayilariniToplama {
+    public static void main(String[] args) {
+        int number;
+        int basValue;
+        int total=0;
+
+
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Lütfen sayı giriniz : ");
+        number= inp.nextInt();
+
+
+        int tempNumber = number;
+
+        while(tempNumber!=0){
+            basValue=tempNumber%10;
+            total +=basValue;
+
+            tempNumber/=10;
+
+        }
+        System.out.print("Girdiğiniz sayının basamakları toplamı : " + total);
+
+
+    }
+}
