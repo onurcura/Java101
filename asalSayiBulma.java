7d7f3b5b2288fb241b73a87ba0f30af420129dbf
Onur Cura <onurcuraa@gmail.com>
2022-11-11 21:09:49 +0300

    Proje yapıldı.

--- asalSayiBulma.java
+++ asalSayiBulma.java
@@ -1,6 +1,8 @@
 public class asalSayiBulma {
     public static void main(String[] args) {
 
+        //Java ile 1 - 100 arasındaki asal sayıları ekrana yazdıran program.
+
         for (int i = 1; i <= 100; i++) {
             int sayac = 0;
             for (int j = i; j >= 1; j--) {
